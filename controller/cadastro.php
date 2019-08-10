1eacc2a3121b81681d42ff5bef6f2d4badf6b1f0
bdguypsy <bdguypsy@gmail.com>
2019-08-10 09:46:25 -0300

    Cadastro app

--- controller/cadastro.php
+++ controller/cadastro.php
@@ -137,7 +137,7 @@
 		AND
 			CEP_END = '" . $cep . "'
 		LIMIT 1")) {
-		$result->commit();
+		$mysqli->commit();
 		$row = $result->fetch_array(MYSQLI_ASSOC);
 		echo $row;
 		$idEndereco = $row["ID_END"];
