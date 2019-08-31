bad52edc5f821e288876845e1c9e14c36e65cc24
bdguypsy <bdguypsy@gmail.com>
2019-08-31 07:42:54 -0300

    Inserindo o SweetAlert

--- app_assets/js/cadastro.js
+++ app_assets/js/cadastro.js
@@ -13,15 +13,13 @@ $(document).ready(function(){
 		var ddd = $("#txtDDD").val();
 		var celular = $("#txtCelular").val();
 		var email = $("#txtEmail").val();
-		if (nome == "" || nome == null || nome == undefined || usuario == "" || usuario == null || usuario == undefined || senha == "" || senha == null || senha == undefined || cep == "" || cep == null || cep == undefined || uf == "" || uf == null || uf == undefined || logradouro == "" || logradouro == null || logradouro == undefined || numero == "" || numero == null || numero == undefined || complemento == "" || complemento == null || complemento == undefined || bairro == "" || bairro == null || bairro == undefined || cidade == "" || cidade == null || cidade == undefined || ddd == "" || ddd == null || ddd == undefined || celular == "" || celular == null || celular == undefined || email == "" || email == null || email == undefined) {
-			
+		if (nome == "" || nome == null || nome == undefined || usuario == "" || usuario == null || usuario == undefined || senha == "" || senha == null || senha == undefined || cep == "" || cep == null || cep == undefined || uf == "" || uf == null || uf == undefined || logradouro == "" || logradouro == null || logradouro == undefined || numero == "" || numero == null || numero == undefined || bairro == "" || bairro == null || bairro == undefined || cidade == "" || cidade == null || cidade == undefined || ddd == "" || ddd == null || ddd == undefined || celular == "" || celular == null || celular == undefined || email == "" || email == null || email == undefined) {
+			Swal.fire({
+				type: 'error',
+				title: 'Erro!',
+				text: 'Preencha todos os campos obrigatórios!'
+			});
 		}
-		Swal.fire({
-			type: 'error',
-		  	title: 'Oops...',
-		  	text: 'Something went wrong!',
-		  	footer: '<a href>Why do I have this issue?</a>'
-		});
 		/*$.ajax({
 			type: "POST",
 			url: "http://pizzafast.minc.net.br/pizzafast-repotest/controller/cadastro.php",
