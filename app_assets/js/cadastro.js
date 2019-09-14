e28fa98f1929a9d21dd8e5e8eea5dd3d2d522150
bdguypsy <bdguypsy@gmail.com>
2019-09-14 06:49:23 -0300

    Inserindo validações de dados cadastrais

--- app_assets/js/cadastro.js
+++ app_assets/js/cadastro.js
@@ -1,3 +1,31 @@
+function Onlynumbers(e) {
+	var tecla = new Number();
+	if (window.event) {
+		tecla = e.keyCode;
+	} else if (e.which) {
+		tecla = e.which;
+	} else {
+		return true;
+	}
+	if ((tecla >= "97") && (tecla <= "122")) {
+		return false;
+	}
+}
+
+function Onlychars(e) {
+	var tecla = new Number();
+	if (window.event) {
+		tecla = e.keyCode;
+	} else if (e.which) {
+		tecla = e.which;
+	} else {
+		return true;
+	}
+	if ((tecla >= "48") && (tecla <= "57")) {
+		return false;
+	}
+}
+
 $(document).ready(function(){
 	$("#btnCadastrar").click(function(){
 		var nome = $("#txtNome").val();
